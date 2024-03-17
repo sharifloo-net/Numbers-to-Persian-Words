d1129a04cf3a846504151bce2684cfd3ca9dfe35
sharifloo-net <sharifloo.net@gmail.com>
2024-03-17 17:19:04 +0330

    Add a basic convert decimal numbers to Persian words ( it has some bugs:\ )

--- num-to-words.js
+++ num-to-words.js
@@ -48,55 +48,118 @@ function convertNumberToPersianWords(number) {
         'نهصد',
     ];
     const persianThousands = ['', 'هزار', 'میلیون', 'میلیارد'];
-
+    const persianDecimalThousands = [
+        'دهم',
+        'صدم',
+        'هزارم',
+        'ده هزارم',
+        'صد هزارم',
+        'میلیونم',
+    ];
+    const pushNumInSegments = (num, segmentsType) => {
+            while (num > 0) {
+                if (segmentsType == decimalSegments) {
+                    //? how to select zeros at beginning of a var?
+                    segmentsType.push(String(num % 1000));
+                } else segmentsType.push(num % 1000);
+                num = Math.floor(num / 1000);
+            }
+        },
+        convertToWordsLoop = (isInteger = true) => {
+            if (isInteger) return true;
+            return false;
+        };
     let isNegative = false;
+    let words = '';
+    let segments = [],
+        decimalSegments = [];
 
-    if (number === 0) {
-        return persianNumbers[0];
-    } else if (number < 0) {
+    if (number === 0) return persianNumbers[0];
+    else if (number < 0) {
         number = Math.abs(number);
         isNegative = true;
     }
+    if (number === Math.floor(number)) pushNumInSegments(number, segments);
+    else {
+        let integerPart = number.toString().split('.')[0];
+        let decimalPart = number.toString().split('.')[1];
 
-    let words = '';
-    let segments = [];
-
-    while (number > 0) {
-        segments.push(number % 1000);
-        number = Math.floor(number / 1000);
+        pushNumInSegments(integerPart, segments);
+        pushNumInSegments(decimalPart, decimalSegments);
     }
-
-    for (let i = segments.length - 1; i >= 0; i--) {
-        let segment = segments[i];
+    for (
+        i =
+            segments.length -
+            1 +
+            (decimalSegments.length != 0 ? decimalSegments.length : 0);
+        i >= 0;
+        i--
+    ) {
+        let segment = null,
+            decimalSegment = null,
+            backupDecimalSegment = null;
+        if (segments[i - 1] != null) segment = segments[i - 1];
+        else {
+            decimalSegment = backupDecimalSegment = decimalSegments[i]; //* here
+        }
         if (segment === 0) continue;
-
-        let segmentWords = '';
+        let segmentWords = '',
+            decimalSegmentWords = '';
 
         if (segment >= 100) {
             segmentWords += persianHundreds[Math.floor(segment / 100)] + ' و ';
             segment %= 100;
+        } else if (decimalSegment >= 100) {
+            decimalSegmentWords +=
+                persianHundreds[Math.floor(decimalSegment / 100)] + ' و ';
+            decimalSegment %= 100;
         }
-
-        if (segment > 10 && segment < 20) {
-            segmentWords += persianExceptions[segment % 10];
+        //* changed
+        if (segment != null && segment != 0) {
+            if (segment > 10 && segment < 20) {
+                segmentWords += persianExceptions[segment % 10];
+            } else {
+                if (segment >= 10) {
+                    segmentWords +=
+                        persianTens[Math.floor(segment / 10)] + ' و ';
+                    segment %= 10;
+                }
+                if (segment > 0) {
+                    segmentWords += persianNumbers[segment];
+                    segment = 0; //* new code
+                }
+            }
         } else {
-            if (segment >= 10) {
-                segmentWords += persianTens[Math.floor(segment / 10)] + ' و ';
-                segment %= 10;
-            }
-            if (segment > 0) {
-                segmentWords += persianNumbers[segment];
+            if (decimalSegment > 10 && decimalSegment < 20) {
+                decimalSegmentWords += persianExceptions[decimalSegment % 10];
+            } else {
+                if (decimalSegment >= 10) {
+                    decimalSegmentWords +=
+                        persianTens[Math.floor(decimalSegment / 10)] + ' و ';
+                    decimalSegment %= 10;
+                }
+                if (decimalSegment > 0) {
+                    decimalSegmentWords += persianNumbers[decimalSegment] + ' ';
+                }
             }
         }
-
-        if (i > 0) {
-            segmentWords += ' ' + persianThousands[i] + ' و ';
+        if (i > segments.length - 1 + decimalSegments.length) {
+            if (segmentWords != '')
+                segmentWords += ' ' + persianThousands[i] + ' و ';
+            else decimalSegmentWords += ' ' + persianThousands[i] + ' و ';
         }
-
-        words += segmentWords + ' ';
+        if (i === 0 && decimalSegmentWords !== '') {
+            words +=
+                ' ممیز ' +
+                decimalSegmentWords +
+                persianDecimalThousands[
+                    backupDecimalSegment.toString().length - 1
+                ];
+        } else words += segmentWords + ' ';
     }
     if (isNegative) words = 'منفی ' + words;
+
     return words.trim();
 }
 
-console.log(convertNumberToPersianWords(1234));
+console.log(convertNumberToPersianWords(12.023));
